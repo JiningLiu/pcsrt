e6328ccffd17cef43ace3da986be062be8b11860
Jining Liu <hello@jining.dev>
2025-12-29 16:44:31 -0600

    installscript update

--- install.sh
+++ install.sh
@@ -24,7 +24,10 @@ cd
 sudo rm -r "$PCSRT_GSTREAMER_DEBS"
 
 # install pcsrt binary
-TAG=$(curl -s https://api.github.com/repos/JiningLiu/pcsrt/releases/latest | jq -r '.tag_name')
+TAG=$(curl -s https://api.github.com/repos/JiningLiu/pcsrt/releases/latest \
+    | grep '"tag_name":' \
+    | head -n1 \
+    | sed -E 's/.*"([^"]+)".*/\1/')
 sudo curl -fSL https://github.com/JiningLiu/pcsrt/releases/download/$TAG/pcsrt -o /usr/bin/pcsrt
 sudo chmod +x /usr/bin/pcsrt
 
